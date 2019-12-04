76878b8ae10fa8907efbe0a20109fec7c6f4e120
Jrr-Tolkien <jsvale.guerrero@gmail.com>
2019-12-04 15:45:57 -0600

    some fixing

--- chat room/config.js
+++ chat room/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB_URI || 'mongodb+srv://jsvale-guerrero:mierda358@progra-iii-cwrlz.mongodb.net/proyecto?retryWrites=true&w=majority',
+    db: process.env.MONGODB_URI || '<mongoDB connection link>',
     SECRET_TOKEN: 'miclavedetokens'
 }
